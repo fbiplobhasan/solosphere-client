bca8c2d29d89f77f8926ec26a3739e514c30613a
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-08-29 22:45:32 +0600

    add more functionality

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,15 +1,18 @@
 import axios from "axios";
-import { format } from "date-fns";
-import { useEffect, useState } from "react";
-
+import { compareAsc, format } from "date-fns";
+import { useContext, useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+import toast from "react-hot-toast";
 
 const JobDetails = () => {
+  const { user } = useContext(AuthContext);
   const [startDate, setStartDate] = useState(new Date());
   const { id } = useParams();
   const [job, setJob] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchJobData();
@@ -19,7 +22,7 @@ const JobDetails = () => {
       `${import.meta.env.VITE_API_URL}/job/${id}`
     );
     setJob(data);
-    setStartDate(new Date(data.deadline));
+    // setStartDate(new Date(data.deadline));
   };
 
   const {
@@ -33,6 +36,62 @@ const JobDetails = () => {
     _id,
     buyer,
   } = job || {};
+
+  // Handle form submit
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const price = form.price.value;
+    const email = user?.email;
+    const comment = form.comment.value;
+    const jobId = _id;
+
+    // 0. check bid permissions validation
+    if (user?.email === buyer?.email)
+      return toast.error("Action not permitted");
+
+    // 1. deadline crossed validation
+    if (compareAsc(new Date(), new Date(deadline)) === 1)
+      return toast.error("Deadline crossed, Bidding Forbidden.");
+
+    // 2. maximum price_range validation
+    if (price > max_price)
+      return toast.error("Offer less or atleast qual to maximum price.");
+
+    // offered deadline is within sellers deadline validation
+    // buyer deya deadline er moddhe complete korar validation
+    if (compareAsc(new Date(startDate), new Date(deadline)) === 1)
+      return toast.error("Offer a date within deadline.");
+
+    const bidsData = {
+      price,
+      email,
+      comment,
+      deadline: startDate,
+      jobId,
+      title,
+      category,
+      status: "Pending",
+      buyer: buyer?.email,
+    };
+
+    try {
+      // 1. make a post req with axios
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/add-bid`,
+        bidsData
+      );
+      // 2. Reset form
+      e.target.reset();
+      // 3. show toast modal
+      toast.success("Bidding Successfully.");
+      // 4. navigate to my posted page.
+      navigate("/my-bids");
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.response?.data);
+    }
+  };
   return (
     <div className="flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto ">
       {/* Job Details */}
@@ -68,6 +127,7 @@ const JobDetails = () => {
             </div>
             <div className="rounded-full object-cover overflow-hidden w-14 h-14">
               <img src={buyer?.photo} alt="" />
+              {/* referrerPolicy="no-referrer" */}
             </div>
           </div>
           <p className="mt-6 text-lg font-bold text-gray-600 ">
@@ -81,7 +141,7 @@ const JobDetails = () => {
           Place A Bid
         </h2>
 
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label className="text-gray-700 " htmlFor="price">
@@ -104,6 +164,7 @@ const JobDetails = () => {
                 id="emailAddress"
                 type="email"
                 name="email"
+                defaultValue={user?.email}
                 disabled
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
